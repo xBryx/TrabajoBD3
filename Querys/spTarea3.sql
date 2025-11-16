1744d2b76a12e8bf69629dc5e56026e95206eadc
watson234 <165717299+watson234@users.noreply.github.com>
2025-11-16 09:39:10 -0600

    Update spTarea3.sql

--- Querys/spTarea3.sql
+++ Querys/spTarea3.sql
@@ -43,8 +43,6 @@ BEGIN
 	SET @CodigoResultado = 1;
 	DECLARE @IdFinca INT;
 
-	
-
 	BEGIN TRY
 		SELECT @IdFinca = pr.id   --idfinca va a ser id de la tabla propiedad
 		FROM dbo.Propiedad AS pr
@@ -69,4 +67,209 @@ BEGIN
 	END CATCH
 END;
 GO
+CREATE PROCEDURE dbo.InsertarPropietario
+(
+	@Nombre NVARCHAR(128),
+	@ValorIdentidad NVARCHAR(20),
+	@Email NVARCHAR(20),
+	@Telefono NVARCHAR(20),
+	@CodigoResultado INT OUTPUT
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET @CodigoResultado=1;
 
+	BEGIN TRY
+		BEGIN TRAN
+			INSERT INTO dbo.Propietario(
+							Nombre,
+							ValorDocumentoIdentidad,
+							Email,
+							Telefono)
+			VALUES(
+					@Nombre,
+					@ValorIdentidad,
+					@Email,
+					@Telefono
+					)
+		COMMIT;
+
+	END TRY
+
+	BEGIN CATCH
+		 IF @@TRANCOUNT > 0
+            ROLLBACK;   
+
+        SET @CodigoResultado = -1;  
+        RETURN;
+	END CATCH
+END
+
+GO 
+
+CREATE PROCEDURE dbo.SP_InsertarPropiedades
+(
+	@NumeroFinca NVARCHAR(20),
+	@Valor MONEY,
+	@Saldo INT,
+	@Fecha DATE,
+	@TipoUso INT,
+	@Zona INT,
+	@Medidor NVARCHAR(20)
+)
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+	DECLARE @CodigoResultado INT;
+	SET @CodigoResultado=1;
+	BEGIN TRY
+
+		IF EXISTS(
+			SELECT 1
+			FROM dbo.Propiedad AS p
+			WHERE p.NumeroFinca = @NumeroFinca
+			)
+		BEGIN
+			SET @CodigoResultado=-1;
+			RETURN
+		END
+
+		IF NOT EXISTS(
+				SELECT 1
+				FROM dbo.TipoUso AS tp
+				WHERE tp.IdTipoUso=@TipoUso
+				)
+		BEGIN
+			SET @CodigoResultado=-1;
+			RETURN
+		END
+
+		IF NOT EXISTS(
+				SELECT 1
+				FROM dbo.TipoLocalizacion AS tl
+				WHERE tl.IdTipoLocalizacion=@Zona
+				)
+		BEGIN
+			SET @CodigoResultado=-1;
+			RETURN
+		END
+
+		BEGIN TRAN
+			INSERT INTO dbo.Propiedad(
+										NumeroFinca,
+										ValorFiscal,
+										SaldoM3,
+										FechaRegistro,
+										TipoUsoId,
+										TipoLocalizacionId,
+										NumeroMedidor)
+			VALUES(
+					@NumeroFinca,
+					@Valor,
+					@Saldo,
+					@Fecha,
+					@TipoUso,
+					@Zona,
+					@Medidor
+					)
+		COMMIT;
+										
+	END TRY
+	BEGIN CATCH
+
+		IF @@TRANCOUNT > 0
+            ROLLBACK;
+
+        SET @CodigoResultado = -1;
+        RETURN;
+	END CATCH
+END
+					
+GO
+
+CREATE PROCEDURE dbo.SP_Asociar
+(
+	@valorDocumento NVARCHAR(20),
+	@numeroFinca NVARCHAR(20),
+	@tipoAsociacion INT
+)
+AS
+BEGIN
+	SET NOCOUNT ON 
+	DECLARE @CodigoResultado INT;
+	SET @CodigoResultado=1;
+	DECLARE @Inicio DATE;
+	SET @INICIO= GETDATE();
+	DECLARE @FIN DATE;
+	SET @FIN=NULL;
+	DECLARE @PrId int;
+	DECLARE @PId int;
+	BEGIN TRY
+		IF NOT EXISTS(
+			SELECT 1
+			FROM dbo.Propietario AS pr
+			WHERE pr.ValorDocumentoIdentidad = @valorDocumento
+			)
+		BEGIN
+			SET @CodigoResultado=-1;
+			RETURN
+		END
+		IF NOT EXISTS(
+			SELECT 1
+			FROM dbo.Propiedad AS p
+			WHERE p.NumeroFinca = @NumeroFinca
+			)
+		BEGIN
+			SET @CodigoResultado=-1;
+			RETURN
+		END
+
+		IF NOT EXISTS(
+				SELECT 1
+				FROM dbo.TipoAsociacion AS ta
+				WHERE ta.Id=@tipoAsociacion
+				)
+		BEGIN
+			SET @CodigoResultado=-1;
+			RETURN
+		END
+
+		SELECT @PrId=Id
+		FROM dbo.Propietario AS pr
+		WHERE pr.ValorDocumentoIdentidad=@valorDocumento
+
+		SELECT @PId=id
+		FROM dbo.Propiedad AS p
+		WHERE p.NumeroFinca = @NumeroFinca
+
+		BEGIN TRAN
+			INSERT INTO dbo.PropietarioPropiedad
+												(
+												Idpropiertario,
+												IdPropiedad,
+												IdTipoAsociacion,
+												FechaInicio,
+												FechaFin
+												)
+			VALUES
+				(
+				@PrId,
+				@PId,
+				@tipoAsociacion,
+				@INICIO,
+				@FIN
+				)
+		COMMIT;
+	END TRY
+
+	BEGIN CATCH
+
+		IF @@TRANCOUNT > 0
+            ROLLBACK;
+
+        SET @CodigoResultado = -1;
+        RETURN;
+	END CATCH
+END

74f683af51200dc06b51a0a388eb2e75e869dcc3
bfsEstudiantec <b.fernandez@estudiantec.cr>
2025-11-19 17:40:23 -0600

    Pagar
    Se añadió la funcionalidad de pagar, es decir se conectó el SP de pagar con la app web, además se creó una ventana que muestra los datos del comprobante y se añadió la lógica para que esta se muestre cuando se hace un pago

--- tarea3BD/Pagar.aspx.designer.cs
+++ tarea3BD/Pagar.aspx.designer.cs
@@ -31,5 +31,23 @@ namespace tarea3BD
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnVolver;
+
+        /// <summary>
+        /// Control lblMensajePago.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensajePago;
+
+        /// <summary>
+        /// Control btnCerrarModal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCerrarModal;
     }
 }
